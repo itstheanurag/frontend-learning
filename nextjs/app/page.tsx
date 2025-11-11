30f87ec0cc0b614a891d1e68543468b9fc70beba
gaurav <gauravanurag36@gmail.com>
2025-11-11 18:01:16 +0530

    feat: nextjs initialize

---
+++ nextjs/app/page.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <main className="min-h-screen max-w-6xl w-full mx-auto">
+      <nav>
+        Logo
+      </nav>
+    </main>
+  );
+};
+
+export default Home;
